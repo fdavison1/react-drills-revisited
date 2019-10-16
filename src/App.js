d015f1a54eb1ae2a9e746a372c00ddd6b48c2ee7
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-16 17:03:23 -0600

    some css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import './App.css'
 import App1 from './Apps/App1/App1'
 import App2 from './Apps/App2/App2'
+import App3 from './Apps/App3/App3'
 
 export default class App extends Component{
   constructor(){
@@ -19,6 +20,8 @@ export default class App extends Component{
         <App1 />
         <hr></hr>
         <App2 />
+        <hr></hr>
+        <App3 />
         <hr></hr>
       </div>
     )
---
+++ src/Apps/App3/App3.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+export default class App3 extends Component{
+    constructor(){
+        super()
+
+        this.state = {
+            arr : ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese'],
+            filtered: ''
+        }
+    }
+
+
+    
+    render(){
+        return(
+            <div>
+                <h2>App-3</h2>
+                <p>Create an app where there is an array of data on state that is then shown on the DOM as a list. There should also be a way for the user to filter what's shown in the list. The array of data can be as simple as an array of strings. The list can be as simple as a list of h2 elements.</p>
+                <input />
+                <h3>{filtered}</h3>
+            </div>
+        )
+    }
+}
