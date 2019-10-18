dfbe9d3385d33c56aee589ca20681b06699c00af
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-18 12:55:30 -0600

    updated App8

--- src/components/App8.jsx
+++ src/components/App8.jsx
@@ -7,15 +7,15 @@ export default class App8 extends React.Component{
         super()
 
         this.state = {
-          planet : ''
+          pokemon : ''
         }
        
     }
     
     componentDidMount(){
-    axios.get('https://cors-anywhere.herokuapp.com/https://swapi.co/api/planets/1').then(res => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/19').then(res => {
         this.setState({
-            planet : res.data
+            pokemon : res.data
             })
         })
     }
@@ -27,12 +27,11 @@ export default class App8 extends React.Component{
                   <div>
                       <h2>App-8</h2>
                       <p>Create an app hitting an API of your choice (swapi.co, birdapi.com, pokeapi, smurfs, marvel api, etc). The API should be hit as soon as the component is finished rendering. The app should set value(s) on state based on results from the API and then show the propertie(s) on state in the DOM.</p>
-                      <h3>Name: {this.state.planet.name}</h3>
-                      <h3>Population: {this.state.planet.population}</h3>
-                      <h3>Terrain: {this.state.planet.terrain}</h3>
-                      <h3>Climate: {this.state.planet.climate}</h3>
-                      <h3>Gravity: {this.state.planet.gravity}</h3>
-                      <h3>Diameter: {this.state.planet.diameter}</h3>
+                      <h3>Name: {this.state.pokemon.name}</h3>
+                      <h3>Order: {this.state.pokemon.order}</h3>
+                      <h3>Height: {this.state.pokemon.height}</h3>
+                      <h3>Weight: {this.state.pokemon.weight}</h3>
+                      <h3>Base Experience: {this.state.pokemon.base_experience}</h3>
                   </div>
                 
         
