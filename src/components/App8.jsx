f41b2be583eeba30757109fd44c96398f44e1443
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-18 13:05:14 -0600

    new feature - App8

--- src/components/App8.jsx
+++ src/components/App8.jsx
@@ -12,8 +12,8 @@ export default class App8 extends React.Component{
        
     }
     
-    componentDidMount(){
-    axios.get('https://pokeapi.co/api/v2/pokemon/19').then(res => {
+    getPokemon(){
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${Math.ceil(Math.random()*964)}`).then(res => {
         this.setState({
             pokemon : res.data
             })
@@ -27,6 +27,7 @@ export default class App8 extends React.Component{
                   <div>
                       <h2>App-8</h2>
                       <p>Create an app hitting an API of your choice (swapi.co, birdapi.com, pokeapi, smurfs, marvel api, etc). The API should be hit as soon as the component is finished rendering. The app should set value(s) on state based on results from the API and then show the propertie(s) on state in the DOM.</p>
+                      <button onClick={()=> this.getPokemon()}>Get Pokemon</button>
                       <h3>Name: {this.state.pokemon.name}</h3>
                       <h3>Order: {this.state.pokemon.order}</h3>
                       <h3>Height: {this.state.pokemon.height}</h3>
