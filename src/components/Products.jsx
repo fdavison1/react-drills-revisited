43a971f11c37c9717d33d5b55f864578d93fc0ed
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-22 16:13:41 -0600

    quick save App10

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 export default class Products extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            pokemon: ['fred']
+        }
+    }
+
+    componentDidMount(){
+        axios
+        .get(`https://pokeapi.co/api/v2/pokemon/`)
+        .then(res => {
+            console.log(res)
+            this.setState({
+                pokemon: res.data.results
+            })
+        })
+    }
 
     render(){
+        let pokemon = this.state.pokemon.map((el, i) => {
+            return <Link key={i} to={`/productdetails/${el.id}`}>{el.name}</Link>
+        })
         return(
-            <div>test</div>
+            <div>
+                <h3>test</h3>
+                {pokemon}
+            </div>
         )
     }
 }
